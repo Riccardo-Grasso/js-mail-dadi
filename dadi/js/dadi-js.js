3aba65752fbb90791bdd5734208a27ee72341022
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-13 19:05:53 +0200

    versione con più dadi

--- dadi/js/dadi-js.js
+++ dadi/js/dadi-js.js
@@ -1,17 +1,49 @@
-const dadoUtente = Math.floor(Math.random() * 6) + 1;
-console.log(`Risultato utente: ${dadoUtente}`);
-alert(`Risultato Utente: ${dadoUtente}`);
+const numeroDadi = parseInt(prompt("Inserire il numero di dadi con cui si vuole giocare:"));
 
-const dadoComputer = Math.floor(Math.random() * 6) + 1;
-console.log(`Risultato Computer: ${dadoComputer}`);
-alert(`Risultato Computer: ${dadoComputer}`);
-
-if (dadoUtente > dadoComputer) {
-    alert(`L'utente ha battuto il computer per ${dadoUtente}-${dadoComputer}`);
-
-} else if (dadoUtente < dadoComputer) {
-    alert(`Il computer ha battuto l'utente per ${dadoComputer}-${dadoUtente}`);
+if (isNaN(numeroDadi)) {
+    alert("Numero dadi non valido, riprovarte.");
 
 } else {
-    alert(`Pareggio: ${dadoUtente}-${dadoComputer}`);
+    console.log(`Numero dadi: ${numeroDadi}`);
+    alert(`Stai giocando con ${numeroDadi} dadi.`);
+
+    const lancioPlayer = [];
+    const lancioComputer = [];
+
+    // Risultati PLAYER
+    for (i = 0; i < numeroDadi; i++) {
+        lancioPlayer.push(Math.floor(Math.random() * 6) + 1);
+    }
+    console.log(lancioPlayer);
+
+    const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+    const punteggioPlayer = lancioPlayer.reduce(reducer);
+
+    console.log("Punteggio Player: " + punteggioPlayer);
+    alert(`Risultato lancio Player: ${lancioPlayer}. Punteggio Totale: ${punteggioPlayer}`);
+
+    // Risultati COMPUTER
+    for (i = 0; i < numeroDadi; i++) {
+        lancioComputer.push(Math.floor(Math.random() * 6) + 1);
+    }
+    console.log(lancioComputer);
+
+    const punteggioComputer = lancioComputer.reduce(reducer);
+    console.log("Punteggio Computer: " + punteggioComputer);
+    alert(`Risultato lancio Computer: ${lancioComputer}. Punteggio Totale: ${punteggioComputer}`);
+
+    //Controllo risultati
+    if (punteggioPlayer > punteggioComputer) {
+        console.log("Vince: Player");
+        alert(`Il Player ha battuto il Computer per ${punteggioPlayer}-${punteggioComputer}`);
+
+    } else if (punteggioPlayer < punteggioComputer) {
+        console.log("Vince: Computer");
+        alert(`Il Computer ha battuto il Player per ${punteggioPlayer}-${punteggioComputer}`);
+
+    } else {
+        console.log("Pareggio");
+        alert(`Pareggio: ${punteggioPlayer}-${punteggioComputer}`);
+    }
 }
